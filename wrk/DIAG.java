7941c729de03293f78322849964c27d1f6057c78
it-lo <it-lo@WMH-SPECTRE.fritz.box>
2026-01-30 05:51:36 +0100

    wrk updated

---
+++ wrk/DIAG.java
@@ -0,0 +1,42 @@
+/* --------------------------------------------------------------------------
+ * Project: CAR OSGi
+ * --------------------------------------------------------------------------
+ * Use of this software is subject to license terms. All Rights Reserved. 
+ * -------------------------------------------------------------------------- */
+
+package biz.car.osgi.bundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import biz.car.ILogger;
+import biz.car.XLogger;
+import biz.car.config.ACS;
+
+/**
+ * The logger for diagnostic purposes.
+ *
+ * @version 2.0.0 15.01.2026 11:57:38
+ */
+public class DIAG {
+
+	/**
+	 * The default diagnose logger. <br>
+	 * The name of the logger can be set in the application properties file. The key
+	 * is 'diagnoseLogger'.
+	 */
+	public static final XLogger LOG;
+	
+	static {
+		String l_name = ACS.APP.getString(VAL.diagnoseLogger);
+		Logger l_logger = LoggerFactory.getLogger(l_name);
+		LOG = new ILogger(l_logger);
+	}
+
+	/**
+	 * Creates a default <code>DIAG</code> instance.
+	 */
+	private DIAG() {
+		super();
+	}
+}
